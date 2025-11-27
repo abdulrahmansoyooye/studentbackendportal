78a2260188ec1e1dbfa7394565f485b9b36eeff1
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2025-11-27 14:50:05 +0100

    Add photo field to new ID card creation

--- controllers/IdCard.js
+++ controllers/IdCard.js
@@ -28,6 +28,7 @@ export const requestIdCard = async (req, res) => {
       department: user.department,
       level: user.level,
       email: user.email,
+      photo: "user.photo",
       status: "pending",
     });
 
